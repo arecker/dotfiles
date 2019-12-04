93e4b42e678cf7aadec25323c6410d19034d0d1d
Alex Recker <alex@reckerfamily.com>
2019-12-04 10:46:37 -0600

    change

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,12 +4,12 @@ log() {
     echo "bootstrap.sh: $1"
 }
 
-log "running bootstrap as $(whoami)@$(hostname) from $(pwd)"
+log "running bootstrap as $(whoami) on $(hostname)"
 
 if [ ! -d "$HOME/src/dotfiles" ]; then
     log "cloning dotfiles"
     mkdir -p "$HOME/src"
-    git clone https://github.com/arecker/dotfiles.git "$HOME/src/dotfiles"
+    git clone "https://github.com/arecker/dotfiles.git" "$HOME/src/dotfiles"
 else
     log "dotfiles already cloned"
 fi
