f2e4a786d18d10de21fc5393c419f911193a5fda
Alex Recker <alex@reckerfamily.com>
2021-04-14 22:55:33 -0500

    Fix orientation of images

--- scripts/bin/resize.bash
+++ scripts/bin/resize.bash
@@ -8,20 +8,20 @@ log() {
 check_installed() {
     if ! command -v "$1" &> /dev/null
     then
-	log "$1 command not available!"
-	exit 1
+        log "$1 command not available!"
+        exit 1
     fi
 }
 
 is_too_big() {
     local max="800"
     if [[ "$(identify -format %wx%h $1)" =~ ^([0-9]+)x([0-9]+) ]]; then
-	local height="${BASH_REMATCH[1]}"
-	local width="${BASH_REMATCH[2]}"
-	[[ "$width" -gt "$max" || "$height" -gt "$max" ]]
+        local height="${BASH_REMATCH[1]}"
+        local width="${BASH_REMATCH[2]}"
+        [[ "$width" -gt "$max" || "$height" -gt "$max" ]]
     else
-	log "error checking size of $1"
-	exit 1
+        log "error checking size of $1"
+        exit 1
     fi
 }
 
@@ -31,7 +31,7 @@ done
 
 for image in $(find ~/src/blog/images -type f); do
     if is_too_big "$image"; then
-	log "resizing $image"
-	convert "$image" -resize "800x800" "$image"
+        log "resizing $image"
+        convert "$image" -resize "800x800" -auto-orient "$image"
     fi
 done
