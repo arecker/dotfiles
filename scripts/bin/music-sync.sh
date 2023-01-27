ebdac12c229bd38bd261ed374a6eff4ff6293297
Alex Recker <alex@reckerfamily.com>
2023-01-27 11:00:54 -0600

    Fix music sync script

--- scripts/bin/music-sync.sh
+++ scripts/bin/music-sync.sh
@@ -8,5 +8,5 @@ SRC="nas.local:/volume1/media/music/"
 TRG="$HOME/Music/"
 
 log "syncing $SRC to $TRG"
-rsync -r --progress --size-only "$SRC" "$TRG"
+rsync -r --progress --size-only --exclude '*@eaDir*' "$SRC" "$TRG"
 log "sync finished"
